247b8b4654f6a7026b2c43f5622afc5d3fef8932
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-03-21 20:30:23 +0100

    :bug: Fix latest develop build
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- update.sh
+++ update.sh
@@ -29,7 +29,7 @@ dockerRepo="monogramm/docker-etherpad"
 latests=( $( curl -fsSL 'https://api.github.com/repos/ether/etherpad-lite/tags' |tac|tac| \
 	grep -oE '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | \
 	sort -urV )
-	latest )
+	develop )
 
 # Remove existing images
 echo "reset docker images"
