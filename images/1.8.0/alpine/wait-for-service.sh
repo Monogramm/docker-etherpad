8ef8c5b467c46345656d4b3cc740673babb2067a
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-03-31 04:20:32 +0200

    :bug: Set VOLUME and copy wait for scripts
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

---
+++ images/1.8.0/alpine/wait-for-service.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+set -e
+
+###########################################################
+# Functions
+
+
+log() {
+    MESSAGE=$1
+
+    echo "[$0] [$(date +%Y-%m-%dT%H:%M:%S)] ${MESSAGE}"
+}
+
+# wait for service to be reachable
+wait_for_service() {
+    WAIT_FOR_ADDR=${1}
+    if [ -z "${WAIT_FOR_ADDR}" ]; then
+        log "Missing service's address to wait for!"
+        exit 1
+    fi
+
+    WAIT_FOR_PORT=${2}
+    if [ -z "${WAIT_FOR_PORT}" ]; then
+        log "Missing service's port to wait for!"
+        exit 1
+    fi
+
+    WAIT_TIME=0
+    WAIT_STEP=${3:-10}
+    WAIT_TIMEOUT=${4:--1}
+
+    while ! nc -z "${WAIT_FOR_ADDR}" "${WAIT_FOR_PORT}" ; do
+        if [ "${WAIT_TIMEOUT}" -gt 0 ] && [ "${WAIT_TIME}" -gt "${WAIT_TIMEOUT}" ]; then
+            log "Service '${WAIT_FOR_ADDR}:${WAIT_FOR_PORT}' was not available on time!"
+            exit 1
+        fi
+
+        log "Waiting service '${WAIT_FOR_ADDR}:${WAIT_FOR_PORT}'..."
+        sleep "${WAIT_STEP}"
+        WAIT_TIME=$(( WAIT_TIME + WAIT_STEP ))
+    done
+    log "Service '${WAIT_FOR_ADDR}:${WAIT_FOR_PORT}' available."
+}
+
+wait_for_services() {
+    if [ -z "${WAIT_FOR_SERVICES}" ]; then
+        log "Missing env var 'WAIT_FOR_SERVICES' defining services to wait for!"
+        exit 1
+    fi
+
+    for S in ${WAIT_FOR_SERVICES}; do
+        WAIT_FOR_ADDR=$(echo "${S}" | cut -d: -f1)
+        WAIT_FOR_PORT=$(echo "${S}" | cut -d: -f2)
+
+        wait_for_service "${WAIT_FOR_ADDR}" "${WAIT_FOR_PORT}" "${WAIT_STEP}" "${WAIT_TIMEOUT}"
+    done
+
+}
+
+###########################################################
+# Runtime
+
+wait_for_services
+
+exec "$@"
