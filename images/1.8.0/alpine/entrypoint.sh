56483c2f375b3bd1b7aff862be4f4f540c720aec
Mathieu Brunot <mathieu.brunot@monogramm.io>
2020-06-18 02:25:48 +0200

    🎨 Regroup wait functions in entrypoint (#8)
    * :art: Regroup wait functions in entrypoin
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>
    
    * :bug: Fix entrypoint location
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>
    
    * :wrench: Set exec permissions on entrypoint
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>
    
    * :bug: Fix permissions of entrypoint
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>
    
    * :bug: Remove command override
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>
    
    * :art: Upgrade compose syntax version
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/1.8.0/alpine/entrypoint.sh
+++ images/1.8.0/alpine/entrypoint.sh
@@ -1,14 +1,49 @@
 #!/bin/sh
+
 set -e
 
-###########################################################
+# -------------------------------------------------------------------
 # Functions
 
-
 log() {
-    MESSAGE=$1
+  echo "[$0] [$(date +%Y-%m-%dT%H:%M:%S%:z)] $@"
+}
 
-    echo "[$0] [$(date +%Y-%m-%dT%H:%M:%S)] ${MESSAGE}"
+# wait for file/directory to exists
+wait_for_file() {
+    WAIT_FOR_FILE=${1}
+    if [ -z "${WAIT_FOR_FILE}" ]; then
+        log "Missing file path to wait for!"
+        exit 1
+    fi
+
+    WAIT_TIME=0
+    WAIT_STEP=${2:-10}
+    WAIT_TIMEOUT=${3:--1}
+
+    while [ ! -d "${WAIT_FOR_FILE}" ] ; do
+        if [ "${WAIT_TIMEOUT}" -gt 0 ] && [ "${WAIT_TIME}" -gt "${WAIT_TIMEOUT}" ]; then
+            log "File '${WAIT_FOR_FILE}' was not available on time!"
+            exit 1
+        fi
+
+        log "Waiting file '${WAIT_FOR_FILE}'..."
+        sleep "${WAIT_STEP}"
+        WAIT_TIME=$(( WAIT_TIME + WAIT_STEP ))
+    done
+    log "File '${WAIT_FOR_FILE}' exists."
+}
+
+wait_for_files() {
+    if [ -z "${WAIT_FOR_FILES}" ]; then
+        log "Missing env var 'WAIT_FOR_FILES' defining files to wait for!"
+        exit 1
+    fi
+
+    for H in ${WAIT_FOR_FILES}; do
+        wait_for_file "${H}" "${WAIT_STEP}" "${WAIT_TIMEOUT}"
+    done
+
 }
 
 # wait for service to be reachable
@@ -57,9 +92,14 @@ wait_for_services() {
 
 }
 
-###########################################################
+# -------------------------------------------------------------------
 # Runtime
 
-wait_for_services
+# Wait for external databases
+if [ -n "${DB_HOST}" ] && [ -n "${DB_PORT}" ]; then
+    wait_for_service "${DB_HOST}" "${DB_PORT}"
+fi
 
+log "Starting..."
 exec "$@"
+
